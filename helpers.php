bf051e032b1b7f14656272c7b5c51553757dc7b1
Wilfried H KANA <69805539+KN237@users.noreply.github.com>
2025-01-19 11:41:17 +0100

    first commit

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get a path and return the absolute path
+ *
+ * @param string $path
+ * @return string
+ */
+function basePath($path = '')
+{
+
+    return __DIR__ . '/' . $path;
+}
